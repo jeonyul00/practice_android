a4a679345baf5afae3584e465c7929686e064177
jeon yul <jeonyul.citylabs@gmail.com>
2023-08-17 17:26:50 +0900

    feat : detail layout value

---
+++ app/src/main/java/com/jeonyul/shoppi_android/GlideModule.kt
@@ -0,0 +1,9 @@
+package com.jeonyul.shoppi_android
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class GlideModule:AppGlideModule() {
+
+}
